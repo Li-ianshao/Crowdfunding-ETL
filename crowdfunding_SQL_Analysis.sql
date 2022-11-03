de0259a8dd5c9759cd0a59239c484b5b51a3f6fa
poi7415778@gmail.com <ian0920365709>
2022-11-03 09:40:38 -0500

    20221103

--- crowdfunding_SQL_Analysis.sql
+++ crowdfunding_SQL_Analysis.sql
@@ -4,6 +4,7 @@
 SELECT backers_count,
 	   cf_id
 FROM campaign
+WHERE outcome = 'live'
 ORDER BY cf_id DESC
 
 
@@ -19,11 +20,12 @@ ORDER BY cf_id DESC
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
-SELECT first_name,last_name,email,goal AS "Remaining Goal Amount"
---INTO email_contacts_remaining_goal_amount
+SELECT first_name,last_name,email,goal - pledged AS "Remaining Goal Amount"
+INTO email_contacts_remaining_goal_amount
 FROM contacts
 JOIN campaign
 ON campaign.contact_id = contacts.contact_id
+WHERE outcome = 'live'
 ORDER BY "Remaining Goal Amount" DESC
 
 
@@ -35,13 +37,14 @@ ORDER BY "Remaining Goal Amount" DESC
 -- Create a table, "email_backers_remaining_goal_amount" that contains the email address of each backer in descending order, 
 -- and has the first and last name of each backer, the cf_id, company name, description, 
 -- end date of the campaign, and the remaining amount of the campaign goal as "Left of Goal". 
-SELECT email,first_name,last_name,cf_id,company_name,description,end_date,goal AS "Left of Goal"
+SELECT email,first_name,last_name,backers.cf_id,company_name,description,end_date,goal - pledged AS "Left of Goal"
 INTO email_backers_remaining_goal_amount
-FROM contacts
+FROM backers
 JOIN campaign
-ON campaign.contact_id = contacts.contact_id
-
+ON campaign.cf_id = backers.cf_id
+WHERE outcome = 'live'
+ORDER BY email DESC
 
 -- Check the table
 
-
+SELECT * FROM email_contacts_remaining_goal_amount
